b34131a57097014445c967ef7c080ee9d530c60a
if1live <libsora25@gmail.com>
2018-07-20 18:51:38 +0900

    스트림 이용해서 이동 패킷 전송하기

--- cli/Assets/Game/InputSystem/InputManager.cs
+++ cli/Assets/Game/InputSystem/InputManager.cs
@@ -11,20 +11,17 @@ namespace Assets.Game.InputSystem
     {
         public static InputManager Instance = null;
 
-        // action은 될수있는한 빨리 처리되면 좋겠다
-        // 예를 들면 공격 명령같은거
-        // 자주 발생하지 않으니 즉시 처리해도 부하가 심하지 않을것이다
-        // TODO command가 한 프레임에 2개 발생하면 어떻게 처리되는가? 스트림이 알아서 잘 해주나?
-        ReactiveProperty<InputAction> command = new ReactiveProperty<InputAction>(InputAction.CreateNone());
-        IObservable<InputAction> CommandObservable {
-            get { return command.Where(a => a.type != InputActionTypes.None).AsObservable(); }
-        }
+        public const int SEND_RATE = 15;
 
-        // 이동 명령은 적당한 주기로 처리하기
-        // 같은 방향으로 계속 이동하고 있으면 반복으로 보내지 않기
-        // 방향이 유지되면 갱신할 필요가 없으니까
-        Queue<InputAction> moveQueue = new Queue<InputAction>();
-        InputAction lastMoveAction = InputAction.CreateMove(0, 0);
+        IObservable<InputAction> CommandObservable {
+            get { return command.Where(a => a.type == InputActionTypes.Command).AsObservable(); }
+        }
+        ReactiveProperty<InputAction> command = new ReactiveProperty<InputAction>(InputAction.CreateNone());
+
+        IObservable<InputAction> MoveObserable {
+            get { return move.Where(a => a.type == InputActionTypes.Move).AsObservable(); }
+        }
+        ReactiveProperty<InputAction> move = new ReactiveProperty<InputAction>(InputAction.CreateNone());
 
         public void PushCommand(InputAction action)
         {
@@ -33,15 +30,7 @@ namespace Assets.Game.InputSystem
 
         public void PushMove(InputAction action)
         {
-            // 클라에서의 마지막 이동 요청은 서버로 계속 유지된다
-            // 방향이 같은 경우 굳이 요청을 다시할 필요 없다
-            if(lastMoveAction == action)
-            {
-                return;
-            }
-
-            moveQueue.Enqueue(action);
-            lastMoveAction = action;
+            move.SetValueAndForceNotify(action);
         }
 
         private void Awake()
@@ -54,10 +43,25 @@ namespace Assets.Game.InputSystem
         {
             var conn = ConnectionManager.Instance.Conn;
 
+            // action은 될수있는한 빨리 처리되면 좋겠다
+            // 예를 들면 공격 명령같은거
+            // 자주 발생하지 않으니 즉시 처리해도 부하가 심하지 않을것이다
+            // TODO command가 한 프레임에 2개 발생하면 어떻게 처리되는가? 스트림이 알아서 잘 해주나?
             CommandObservable.Subscribe(action =>
             {
                 SendCommandPacket(conn, action);
             });
+
+            // 이동 명령은 적당한 주기로 처리하기
+            // 같은 방향으로 계속 이동하고 있으면 반복으로 보내지 않기
+            // 방향이 유지되면 갱신할 필요가 없으니까
+            const int moveInterval = 1000 / SEND_RATE;
+            MoveObserable.ThrottleFirst(TimeSpan.FromMilliseconds(moveInterval))
+                .DistinctUntilChanged()
+                .AsObservable().Subscribe(action =>
+                {
+                    SendMovePacket(conn, action);
+                });
         }
 
         private void OnDestroy()
@@ -72,7 +76,7 @@ namespace Assets.Game.InputSystem
             conn.Off(Events.INPUT_MOVE);
             conn.Off(Events.INPUT_COMMAND);
         }
-        
+
         void SendMovePacket(Connection conn, InputAction action)
         {
             var p = new InputMovePacket
