02c97c8662730707cea1c4173a9c94ce09fb6203
if1live <libsora25@gmail.com>
2018-08-01 23:34:04 +0900

    add sln

---
+++ Shigure/Client.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Shigure
+{
+    public class Client
+    {
+        public void Run(string[] args)
+        {
+            // TODO websocket
+        }
+    }
+}
---
+++ ShigureRunner/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Shigure;
+
+namespace ShigureRunner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new Client();
+            client.Run(args);
+        }
+    }
+}
